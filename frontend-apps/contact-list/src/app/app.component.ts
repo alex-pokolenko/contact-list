0cd781dbde756a3859ab4fca1c9f0ef403665043
Alexandr Pokolenko <alexandr.pokolenko@vrpconsulting.com>
2018-06-26 16:05:10 +0300

    use slds

--- frontend-apps/contact-list/src/app/app.component.ts
+++ frontend-apps/contact-list/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+declare global {
+  interface Window {
+    sldsConfig: {
+      sldsIconsPath: string
+    };
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  /**
+   * TODO: move this logic to slds-icon component
+   */
+  private sldsIconsPath = window.sldsConfig.sldsIconsPath;
+
+  sldsPath(icon: string): string {
+    return this.sldsIconsPath + icon;
+  }
 }

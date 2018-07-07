c62d23567a13d8bd1d6023886f7c76d18ffebffa
Alexandr Pokolenko <a_pokolenko@twistellar.com>
2018-07-07 11:30:38 +0300

    add record edit icon

--- frontend-apps/contact-list/src/app/common/ui-components/data-table/data-table.component.ts
+++ frontend-apps/contact-list/src/app/common/ui-components/data-table/data-table.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { TableMessagingService } from './table-messaging.service';
+import { DataTable } from './data-table';
 
 @Component({
   selector: 'app-data-table',
   templateUrl: './data-table.component.html',
-  styleUrls: ['./data-table.component.scss']
+  styleUrls: ['./data-table.component.scss'],
 })
 export class DataTableComponent implements OnInit {
 
